db61326e543c7cefd348fe09ecd6a9d762bac212
IVfromBY <ivfromby@gmail.com>
2022-02-18 02:06:03 +0300

    Добавлен поиск по нескольким столбцам

--- WpfDR/ViewModels/MainWindowViewModel.cs
+++ WpfDR/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Windows.Input;
@@ -8,6 +9,7 @@ using WpfDR.Commands;
 using WpfDR.Model;
 using WpfDR.Service;
 using WpfDR.ViewModels.Base;
+//using System.Windows.Data.Binding;
 
 namespace WpfDR.ViewModels
 {
@@ -15,12 +17,24 @@ namespace WpfDR.ViewModels
     {
 
         private ParserService _Parser;
+        private ICollectionView _MailItensListView { get; set; }
+
         public ObservableCollection<MailItem> MailItems { get; } = new();
+
 
         #region Notifications
 
         private string _Status = ".";
         public string Status { get => _Status; set => Set(ref _Status, value); }
+
+        private string _SearchPhrazeSender = default;
+        public string SearchPhrazeSender { get => _SearchPhrazeSender; set => Set(ref _SearchPhrazeSender, value); }
+
+        private string _SearchPhrazeSubject = default;
+        public string SearchPhrazeSubject { get => _SearchPhrazeSubject; set => Set(ref _SearchPhrazeSubject, value); }
+
+        private string _SearchPhrazeContent = default;
+        public string SearchPhrazeContent { get => _SearchPhrazeContent; set => Set(ref _SearchPhrazeContent, value); }
 
         private bool _ShowProgressBar = false;
         public bool ShowProgressBar { get => _ShowProgressBar; set => Set(ref _ShowProgressBar, value); }
@@ -35,14 +49,49 @@ namespace WpfDR.ViewModels
 
         #region Commands
 
-        private ICommand _loadFile;
-        private bool CanLoadFile(object o) => true;
-        public ICommand LoadFile => _loadFile ??= new LambdaCommand(OnLoadFile, CanLoadFile);
+        private ICommand _loadFileCommand;
+        private bool CanLoadFileCommand(object o) => true;
+        public ICommand LoadFileCommand => _loadFileCommand ??= new LambdaCommand(OnLoadFile, CanLoadFileCommand);
+
+        private ICommand _searchCommand;
+        private bool CanSearchCommand(object o) => MailItems.Count() > 0;
+        public ICommand SearchCommand => _searchCommand ??= new LambdaCommand(OnSearching, CanSearchCommand);
+
+        private ICommand _cancelSearchCommand;
+        private bool CanCancelSearchCommand(object o) => MailItems.Count() > 0;
+
+        public ICommand CancelSearchCommand => _cancelSearchCommand ??= new LambdaCommand(OnCancelSearching, CanCancelSearchCommand);
 
         #endregion
 
-        public MainWindowViewModel(ParserService parser) => _Parser = parser;
+        private void OnCancelSearching(object obj)
+        {
+            SearchPhrazeContent = default;
+            SearchPhrazeSender = default;
+            SearchPhrazeSubject = default;
+            _MailItensListView.Refresh();
+        }
 
+        private void OnSearching(object obj)
+        {
+            _MailItensListView.Refresh();
+        }
+        private bool MailItemFilter(object o)
+        {
+            MailItem mail = o as MailItem;
+
+            return mail.FromAbonent.ToLower().Contains(_SearchPhrazeSender?.ToLower() ?? mail.FromAbonent.ToLower())
+                     && mail.Subject.ToLower().Contains(_SearchPhrazeSubject?.ToLower() ?? mail.Subject.ToLower())
+                     && mail.Content.ToLower().Contains(_SearchPhrazeContent?.ToLower() ?? mail.Content.ToLower())
+                     ;
+        }
+
+        public MainWindowViewModel(ParserService parser)
+        {
+            _Parser = parser;
+            _MailItensListView = System.Windows.Data.CollectionViewSource.GetDefaultView(MailItems);
+            _MailItensListView.Filter = MailItemFilter;
+        }
 
         private async void OnLoadFile(object o)
         {
@@ -68,6 +117,7 @@ namespace WpfDR.ViewModels
                     var progress = new Progress<double>(p => ParseProgress = p);
 
                     var res = await _Parser.ParseTextFileAsync(textFromFile, progress);
+
                     totalLoaded = res.Count();
 
                     foreach (MailItem item in res.GroupBy(g => new { g.FromAbonent, g.Subject, g.DateCreate, g.Content }).Select(g => g.First()))
@@ -84,12 +134,8 @@ namespace WpfDR.ViewModels
             ParseProgress = 0;
             ShowProgressBar = false;
 
-            Status = $"Обработано {totalLoaded}; Удалено уведолений о доставке {notifyDelivery}; Дубликаты: {totalLoaded - notifyDelivery - MailItems.Count}; Показано {MailItems.Count}";
+            Status = $"Обработано {totalLoaded}; Удалено уведомлений о доставке {notifyDelivery}; Дубликаты: {totalLoaded - notifyDelivery - MailItems.Count}; Показано {MailItems.Count}";
         }
-
-
-
-
 
     }
 }
